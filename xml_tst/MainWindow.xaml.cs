f44303548d84f0ddd2912e18b1d500e94b6885ba
TovarnovM <misha198921rip@gmail.com>
2018-06-08 10:18:42 +0300

    xml work

--- xml_tst/MainWindow.xaml.cs
+++ xml_tst/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Xml.Schema;
 using System.Xml.Linq;
+using System.Xml;
 
 namespace xml_tst {
     /// <summary>
@@ -22,13 +23,71 @@ namespace xml_tst {
     public partial class MainWindow: Window {
         public MainWindow() {
             InitializeComponent();
+
+        }
+
+
+        private void BuildTree(TreeView treeView, XDocument doc) {
+            TreeViewItem treeNode = new TreeViewItem {
+                //Should be Root
+                Header = doc.Root.Name.LocalName,
+                IsExpanded = true
+            };
+            treeView.Items.Add(treeNode);
+            BuildNodes(treeNode, doc.Root);
+        }
+
+        private void BuildNodes(TreeViewItem treeNode, XElement element) {
+            foreach (XNode child in element.Nodes()) {
+                switch (child.NodeType) {
+                    case XmlNodeType.Element:
+                        XElement childElement = child as XElement;
+
+                            var h = childElement.Name.ToString();
+                            if (childElement.HasAttributes) {
+                                foreach (var a in childElement.Attributes()) {
+                                    h += " " + a.Name.ToString() + "=" + a.Value.ToString();
+                                    var xname = a.BaseUri;
+                                    var si = a.GetSchemaInfo();
+                                    h += si.SchemaType != null ? $"({si.SchemaType.QualifiedName.Name }) " : "";
+                                }
+
+                            } 
+                            
+                            
+                            TreeViewItem childTreeNode = new TreeViewItem {
+                                //Get First attribute where it is equal to value
+                                Header = h,
+                                //Automatically expand elements
+                                IsExpanded = true
+                            };
+                            treeNode.Items.Add(childTreeNode);
+                            BuildNodes(childTreeNode, childElement);
+
+
+                        break;
+                    case XmlNodeType.Text:
+                        XText childText = child as XText;
+                        treeNode.Items.Add(new TreeViewItem { Header = childText.Value, });
+                        break;
+                }
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e) {
-            var xsd_path = @"D:\qt46\SIM\data\setting.xsd";
+            var xsd_path = @"D:\qt46\SIM\data\settings.xsd";
             var xml_path = @"D:\qt46\SIM\data\some.xml";
             var xdoc = XDocument.Load(xml_path);
-            var i = xdoc.Elements().Count();
+            xdoc = new XDocument(xdoc);
+            XmlSchemaSet schemas = new XmlSchemaSet();
+            schemas.Add(null, xsd_path);
+            string msg = "";
+            xdoc.Validate(schemas, (o, e1) => {
+                msg += e1.Message + Environment.NewLine;
+            },true);
+            btn.Content = msg == "" ? "Document is valid" : "Document invalid: " + msg;
+            
+            BuildTree(tv, xdoc);
         }
     }
 }
--- xml_tst_console/Program.cs
+++ xml_tst_console/Program.cs
@@ -1,22 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using System.Xml.Serialization;
 
 namespace xml_tst_console {
     class Program {
         static void Main(string[] args) {
             var xsd_path = @"D:\qt46\SIM\data\settings.xsd";
             var xml_path = @"D:\qt46\SIM\data\some.xml";
-            var xdoc = XDocument.Load(xsd_path);
+            var xdoc = XDocument.Load(xml_path);
             foreach (var e in GetAllElems(xdoc)
-                                .Where(el=>el.HasAttributes)) {
-                
-                Console.WriteLine(e.Attribute("name")?.Value);
+                                .Where(el=>el.HasAttributes)
+                                .SelectMany(el=>el.Attributes())) {
+                Console.WriteLine($"{e.Name} = {e.Value}");
+                //var val = e.Attribute("xsi:type");
+                //if(val != null)
+                //    Console.WriteLine(val.Value);
 
             }
+
+            //XmlSerializer serializer = new XmlSerializer(typeof(Settings));
+            //Settings set;
+            //using (var reader = new StreamReader(xml_path)) {
+            //    set = (Settings)serializer.Deserialize(reader);
+            //}
+
+
             Console.ReadLine();
         }
 
